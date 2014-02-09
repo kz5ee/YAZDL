1c19bcea48ccda264527d26c1cb57875d5de3a91
Mike <kz5ee@yahoo.com>
2014-02-09 00:08:53 -0600

    Got the brunt of it done and working Need to get the run string in there.

---
+++ frmMain.Designer.vb
@@ -0,0 +1,208 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lstWads = New System.Windows.Forms.ListBox()
+        Me.btnZDoomDir = New System.Windows.Forms.Button()
+        Me.txtPath = New System.Windows.Forms.TextBox()
+        Me.fbdZDoom = New System.Windows.Forms.FolderBrowserDialog()
+        Me.grpNetPlay = New System.Windows.Forms.GroupBox()
+        Me.lblIPValidity = New System.Windows.Forms.Label()
+        Me.txtIPAddress = New System.Windows.Forms.TextBox()
+        Me.radHost = New System.Windows.Forms.RadioButton()
+        Me.radJoin = New System.Windows.Forms.RadioButton()
+        Me.lblRemoteHost = New System.Windows.Forms.Label()
+        Me.cboPlayers = New System.Windows.Forms.ComboBox()
+        Me.lblPlayers = New System.Windows.Forms.Label()
+        Me.btnRunZDoom = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.grpNetPlay.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'lstWads
+        '
+        Me.lstWads.FormattingEnabled = True
+        Me.lstWads.Location = New System.Drawing.Point(12, 12)
+        Me.lstWads.Name = "lstWads"
+        Me.lstWads.Size = New System.Drawing.Size(205, 199)
+        Me.lstWads.TabIndex = 0
+        Me.lstWads.TabStop = False
+        '
+        'btnZDoomDir
+        '
+        Me.btnZDoomDir.Location = New System.Drawing.Point(39, 319)
+        Me.btnZDoomDir.Name = "btnZDoomDir"
+        Me.btnZDoomDir.Size = New System.Drawing.Size(99, 23)
+        Me.btnZDoomDir.TabIndex = 1
+        Me.btnZDoomDir.Text = "ZDoom Folder"
+        Me.btnZDoomDir.UseVisualStyleBackColor = True
+        '
+        'txtPath
+        '
+        Me.txtPath.Location = New System.Drawing.Point(158, 322)
+        Me.txtPath.Name = "txtPath"
+        Me.txtPath.ReadOnly = True
+        Me.txtPath.Size = New System.Drawing.Size(218, 20)
+        Me.txtPath.TabIndex = 2
+        '
+        'fbdZDoom
+        '
+        Me.fbdZDoom.RootFolder = System.Environment.SpecialFolder.MyComputer
+        Me.fbdZDoom.ShowNewFolderButton = False
+        '
+        'grpNetPlay
+        '
+        Me.grpNetPlay.Controls.Add(Me.lblPlayers)
+        Me.grpNetPlay.Controls.Add(Me.cboPlayers)
+        Me.grpNetPlay.Controls.Add(Me.lblRemoteHost)
+        Me.grpNetPlay.Controls.Add(Me.lblIPValidity)
+        Me.grpNetPlay.Controls.Add(Me.txtIPAddress)
+        Me.grpNetPlay.Controls.Add(Me.radHost)
+        Me.grpNetPlay.Controls.Add(Me.radJoin)
+        Me.grpNetPlay.Location = New System.Drawing.Point(266, 12)
+        Me.grpNetPlay.Name = "grpNetPlay"
+        Me.grpNetPlay.Size = New System.Drawing.Size(205, 162)
+        Me.grpNetPlay.TabIndex = 3
+        Me.grpNetPlay.TabStop = False
+        Me.grpNetPlay.Text = "Net Play"
+        '
+        'lblIPValidity
+        '
+        Me.lblIPValidity.AutoSize = True
+        Me.lblIPValidity.Location = New System.Drawing.Point(6, 89)
+        Me.lblIPValidity.Name = "lblIPValidity"
+        Me.lblIPValidity.Size = New System.Drawing.Size(50, 13)
+        Me.lblIPValidity.TabIndex = 3
+        Me.lblIPValidity.Text = "IP Status"
+        Me.lblIPValidity.Visible = False
+        '
+        'txtIPAddress
+        '
+        Me.txtIPAddress.Location = New System.Drawing.Point(7, 66)
+        Me.txtIPAddress.MaxLength = 16
+        Me.txtIPAddress.Name = "txtIPAddress"
+        Me.txtIPAddress.Size = New System.Drawing.Size(94, 20)
+        Me.txtIPAddress.TabIndex = 2
+        '
+        'radHost
+        '
+        Me.radHost.AutoSize = True
+        Me.radHost.Location = New System.Drawing.Point(57, 20)
+        Me.radHost.Name = "radHost"
+        Me.radHost.Size = New System.Drawing.Size(47, 17)
+        Me.radHost.TabIndex = 1
+        Me.radHost.Text = "Host"
+        Me.radHost.UseVisualStyleBackColor = True
+        '
+        'radJoin
+        '
+        Me.radJoin.AutoSize = True
+        Me.radJoin.Checked = True
+        Me.radJoin.Location = New System.Drawing.Point(7, 20)
+        Me.radJoin.Name = "radJoin"
+        Me.radJoin.Size = New System.Drawing.Size(44, 17)
+        Me.radJoin.TabIndex = 0
+        Me.radJoin.TabStop = True
+        Me.radJoin.Text = "Join"
+        Me.radJoin.UseVisualStyleBackColor = True
+        '
+        'lblRemoteHost
+        '
+        Me.lblRemoteHost.AutoSize = True
+        Me.lblRemoteHost.Location = New System.Drawing.Point(6, 50)
+        Me.lblRemoteHost.Name = "lblRemoteHost"
+        Me.lblRemoteHost.Size = New System.Drawing.Size(42, 13)
+        Me.lblRemoteHost.TabIndex = 4
+        Me.lblRemoteHost.Text = "Host IP"
+        '
+        'cboPlayers
+        '
+        Me.cboPlayers.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cboPlayers.FormattingEnabled = True
+        Me.cboPlayers.Items.AddRange(New Object() {"2", "3", "4", "5", "6", "7", "8"})
+        Me.cboPlayers.Location = New System.Drawing.Point(122, 65)
+        Me.cboPlayers.Name = "cboPlayers"
+        Me.cboPlayers.Size = New System.Drawing.Size(62, 21)
+        Me.cboPlayers.TabIndex = 5
+        '
+        'lblPlayers
+        '
+        Me.lblPlayers.AutoSize = True
+        Me.lblPlayers.Location = New System.Drawing.Point(122, 49)
+        Me.lblPlayers.Name = "lblPlayers"
+        Me.lblPlayers.Size = New System.Drawing.Size(51, 13)
+        Me.lblPlayers.TabIndex = 6
+        Me.lblPlayers.Text = "# Players"
+        '
+        'btnRunZDoom
+        '
+        Me.btnRunZDoom.Location = New System.Drawing.Point(505, 397)
+        Me.btnRunZDoom.Name = "btnRunZDoom"
+        Me.btnRunZDoom.Size = New System.Drawing.Size(75, 23)
+        Me.btnRunZDoom.TabIndex = 4
+        Me.btnRunZDoom.Text = "Play"
+        Me.btnRunZDoom.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(38, 400)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ReadOnly = True
+        Me.TextBox1.Size = New System.Drawing.Size(433, 20)
+        Me.TextBox1.TabIndex = 5
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(850, 648)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.btnRunZDoom)
+        Me.Controls.Add(Me.grpNetPlay)
+        Me.Controls.Add(Me.txtPath)
+        Me.Controls.Add(Me.btnZDoomDir)
+        Me.Controls.Add(Me.lstWads)
+        Me.Name = "frmMain"
+        Me.Text = "ZDoom Launcher"
+        Me.grpNetPlay.ResumeLayout(False)
+        Me.grpNetPlay.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents lstWads As System.Windows.Forms.ListBox
+    Friend WithEvents btnZDoomDir As System.Windows.Forms.Button
+    Friend WithEvents txtPath As System.Windows.Forms.TextBox
+    Friend WithEvents fbdZDoom As System.Windows.Forms.FolderBrowserDialog
+    Friend WithEvents grpNetPlay As System.Windows.Forms.GroupBox
+    Friend WithEvents txtIPAddress As System.Windows.Forms.TextBox
+    Friend WithEvents radHost As System.Windows.Forms.RadioButton
+    Friend WithEvents radJoin As System.Windows.Forms.RadioButton
+    Friend WithEvents lblIPValidity As System.Windows.Forms.Label
+    Friend WithEvents lblRemoteHost As System.Windows.Forms.Label
+    Friend WithEvents cboPlayers As System.Windows.Forms.ComboBox
+    Friend WithEvents lblPlayers As System.Windows.Forms.Label
+    Friend WithEvents btnRunZDoom As System.Windows.Forms.Button
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+
+End Class
---
+++ frmMain.vb
@@ -0,0 +1,80 @@
+﻿Imports System.Net
+
+
+Public Class frmMain
+    Public RunString As String = (txtPath.Text & "\zxoom.exe")
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnZDoomDir.Click
+        Dim ZDoomFolder As String
+
+        fbdZDoom.ShowDialog()
+        ZDoomFolder = fbdZDoom.SelectedPath
+
+        If ZDoomFolder <> "" Then
+            GetWadFiles(ZDoomFolder)
+            txtPath.Text = ZDoomFolder
+            My.Settings.ZDoomDirectory = ZDoomFolder
+        End If
+
+
+    End Sub
+
+    Private Sub frmMain_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        If My.Settings.ZDoomDirectory <> "" Then
+            txtPath.Text = My.Settings.ZDoomDirectory
+            GetWadFiles(txtPath.Text)
+        End If
+    End Sub
+
+    Private Sub GetWadFiles(ByVal Folder As String)
+        ' make a reference to a directory
+        Dim di As New IO.DirectoryInfo(Folder)
+        Dim diar1 As IO.FileInfo() = di.GetFiles("*.WAD")
+        Dim dra As IO.FileInfo
+
+        'list the names of all files in the specified directory
+        For Each dra In diar1
+            lstWads.Items.Add(dra)
+        Next
+
+    End Sub
+
+    Private Sub txtIPAddress_LostFocus(sender As Object, e As EventArgs) Handles txtIPAddress.LostFocus
+
+        If IPAddress.TryParse(txtIPAddress.Text, Nothing) = True Then
+            lblIPValidity.ForeColor = Color.Green
+            lblIPValidity.Text = "IP Valid"
+            lblIPValidity.Visible = True
+        Else
+            lblIPValidity.ForeColor = Color.Red
+            lblIPValidity.Text = "IP Invalid"
+            lblIPValidity.Visible = True
+        End If
+
+    End Sub
+
+    Private Sub txtIPAddress_GotFocus(sender As Object, e As EventArgs) Handles txtIPAddress.GotFocus
+        lblIPValidity.Visible = False
+    End Sub
+
+    Private Sub radJoin_CheckedChanged(sender As Object, e As EventArgs) Handles radJoin.CheckedChanged
+        lblPlayers.Enabled = False
+        cboPlayers.Enabled = False
+        txtIPAddress.Enabled = True
+        lblRemoteHost.Enabled = True
+    End Sub
+
+    Private Sub radHost_CheckedChanged(sender As Object, e As EventArgs) Handles radHost.CheckedChanged
+        txtIPAddress.Enabled = False
+        lblRemoteHost.Enabled = False
+        lblPlayers.Enabled = True
+        cboPlayers.Enabled = True
+    End Sub
+
+    Private Sub btnRunZDoom_Click(sender As Object, e As EventArgs) Handles btnRunZDoom.Click
+
+
+        Process.Start(txtPath.Text & "\zdoom.exe")
+    End Sub
+End Class
